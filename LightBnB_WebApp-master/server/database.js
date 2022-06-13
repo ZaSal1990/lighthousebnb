adbc4b21904e1ff25f867a100b23bf55817ddeb8
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-06-13 15:33:30 -0600

    add fns showing all reservations for a user

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -71,7 +71,7 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query('SELECT reservations.* FROM reservations JOIN properties ON reservations.property_id = properties.id JOIN property_reviews ON properties.id = property_reviews.property_id WHERE property_reviews.guest_id = $1 GROUP BY properties.id, reservations.id ORDER BY reservations.start_date LIMIT $2;',[guest_id, limit]).then( result => result.rows);
 }
 exports.getAllReservations = getAllReservations;
 

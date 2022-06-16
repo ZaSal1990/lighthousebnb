9ab4c7fe0169cf65bf9cc95deb167d99feb21abf
ZaSal1990 <khan_zahra4424@hotmail.com>
2022-06-16 10:35:27 -0600

    add screenshots

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,8 +1,8 @@
-SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, avg(property_reviews.rating) AS average_rating 
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE property_reviews.guest_id = 1
+WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date
 LIMIT 10;
